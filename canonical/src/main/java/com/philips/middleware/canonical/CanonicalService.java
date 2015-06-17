bcd62174755eb8abf909d6b88de61be8ba1b1721
Ralph Smeets <ralph.smeets@efocus.nl>
2015-06-17 16:41:04 +0200

    * Updated RestAPI * Put CanonicalService layer between RestAPI (the controller) and CanonicalDAO

--- canonical/src/main/java/com/philips/middleware/canonical/CanonicalService.java
+++ canonical/src/main/java/com/philips/middleware/canonical/CanonicalService.java
@@ -1,5 +1,24 @@
 package com.philips.middleware.canonical;
 
+import java.util.List;
+
 public class CanonicalService {
 
+	final CanonicalDAO canDAO = new CanonicalDAO();
+
+	public String getProductForId(String productId, String language, String country) {
+		return canDAO.getProductForId(language+"_"+country+"_"+productId);
+	}
+
+	public String getFeatureForId(String featureId, String language, String country) {
+		return canDAO.getFeatureForId(language+"_"+country+"_"+featureId);
+	}
+
+	public List<String> getProductIdsForFeatureId(String featureId, String language, String country) {
+		return canDAO.getProductIdsForFeatureId(language+"_"+country+"_"+featureId);
+	}
+
+	public List<String> getFeatureIdsForProductId(String productId, String language, String country) {
+		return canDAO.getFeatureIdsForProductId(language+"_"+country+"_"+productId);
+	}
 }
--- canonical/src/main/java/com/philips/middleware/canonical/RestAPI.java
+++ canonical/src/main/java/com/philips/middleware/canonical/RestAPI.java
@@ -2,21 +2,59 @@ package com.philips.middleware.canonical;
 
 import static spark.Spark.*;
 import com.philips.middleware.canonical.CanonicalDAO;
+import com.google.gson.Gson;
 
 public class RestAPI {
 	public static void main(String[] args) {
 
-		final CanonicalDAO canDAO = new CanonicalDAO();
+		final CanonicalService canonicalService = new CanonicalService();
+		final Gson gson = new Gson();
 
-		get("/product/:product", "application/json", (req, res) -> {
-			String productId = req.params(":product");
-			res.body(canDAO.getProductForId(productId));
+		get("/product/:productid",
+				"application/json",
+				(req, res) -> {
+					String productId = req.params(":productid");
+					String language = req.queryParams("language");
+					String country = req.queryParams("country");
+					System.out.println("You're requesting product " + productId
+							+ " for country " + country + " in language "
+							+ language);
+					res.body(canonicalService.getProductForId(productId,language,country));
+					return res.body();
+				});
+
+		get("/feature/:feature",
+				"application/json",
+				(req, res) -> {
+					String featureId = req.params(":feature");
+					String language = req.queryParams("language");
+					String country = req.queryParams("country");
+					System.out.println("You're requesting feature " + featureId
+							+ " for country " + country + " in language "
+							+ language);
+					res.body(canonicalService.getFeatureForId(featureId,language,country));
+					return res.body();
+				});
+
+		get("productfeatures/:productid", "application/json", (req, res) -> {
+			String productId = req.params(":productid");
+			String language = req.queryParams("language");
+			String country = req.queryParams("country");
+			System.out.println("You're requesting all featureids for product "
+					+ productId + " for country " + country + " in language "
+					+ language);
+			res.body(gson.toJson(canonicalService.getFeatureIdsForProductId(productId,language,country)));
 			return res.body();
 		});
 
-		get("/feature/:feature", "application/json", (req, res) -> {
-			String featureId = req.params(":feature");
-			res.body(canDAO.getFeatureForId(featureId));
+		get("featureproducts/:featureid", "application/json", (req, res) -> {
+			String featureId = req.params(":featureid");
+			String language = req.queryParams("language");
+			String country = req.queryParams("country");
+			System.out.println("You're requesting all productids for feature "
+					+ featureId + " for country " + country + " in language "
+					+ language);
+			res.body(gson.toJson(canonicalService.getProductIdsForFeatureId(featureId,language,country)));
 			return res.body();
 		});
 	}
